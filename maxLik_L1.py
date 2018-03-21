3d78c423ba091f10fb80d54a9d826a6e6ba75255
Saket Jhunjhunwala <sj244@research-henao-02.oit.duke.edu>
2018-03-21 17:55:41 +0000

    Added gumbel sampler

--- maxLik_L1.py
+++ maxLik_L1.py
@@ -50,6 +50,31 @@ def get_zlogprob(z, z_dist):
         raise ValueError("Invalid parameter value for `z_dist`.")
     return logprob
 
+def sample_gumbel(shape, eps=1e-20):
+    """ Sample from Gumbel(0,1)"""
+    U = tf.random_uniform(shape, minval=0,maxval=1)
+    return -tf.log(-tf.log(U+eps) + eps)
+
+def gumble_softmax_sample(logits, temperature):
+    """ Draw a sample from the Gumbel-Softmax distribution """
+    y = logits + sample_gumbel(tf.shape(logits))
+    return tf.nn.softmax(y/temperature)
+
+def gumbel_softmax(logits, temperature, hard=False):
+     """Sample from the Gumbel-Softmax distribution and optionally discretize.
+       Args:
+           logits: [batch_size, n_class] unnormalized log-probs
+           temperature: non-negative scalar
+           hard: if True, take argmax, but differentiate w.r.t. soft sample y
+     Returns:
+         [batch_size, n_class] sample from the Gumbel-Softmax distribution.
+         If hard=True, then the returned sample will be one-hot, otherwise it will
+         be a probabilitiy distribution that sums to 1 across classes
+   """
+
+   y = gumble_softmax_sample(logits, temperature)
+   return y
+
 def standard_normal(shape, **kwargs):
     """Create a standard Normal StochasticTensor."""
     return tf.cast(st.StochasticTensor(
